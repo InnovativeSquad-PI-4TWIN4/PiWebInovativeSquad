c1c3e54820acb18c324fcd46fe7e49bc98c55f21
Fersi <mouadh.fersi@esprit.tn>
2025-02-23 00:29:14 +0100

    identification des roles

---
+++ Frontend/src/components/signin.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../styles/SignIn.scss';
+import { FcGoogle } from 'react-icons/fc';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      const response = await axios.post('http://localhost:3000/users/signin', {
+        email,
+        password
+      });
+  
+      // Supposons que l'API retourne un token
+      if (response.data.token) {
+        localStorage.setItem('token', response.data.token);
+        if (response.data.user.role === "admin") {
+          navigate("/courses");  
+        } else {
+          navigate("/contact");  
+        }
+      } else {
+        setError('Invalid login credentials');
+      }
+    } catch (error) {
+      setError(error.response?.data?.message || 'An error occurred during sign-in.');
+    }
+  };
+  const handleGoogleSignIn = () => {
+    window.location.href = "http://localhost:3000/users/auth/google";
+  };
+
+  return (
+    <div className="signin-container">
+      <div className="signin-box">
+        <h2>Sign In</h2>
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSignIn}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Sign In</button>
+        </form>
+        <p>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+        <button className="google-btn" >
+          <FcGoogle className="google-icon" /> Sign In with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
