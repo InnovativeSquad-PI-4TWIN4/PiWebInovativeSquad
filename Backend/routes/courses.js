d7b71a5d1fd841201eb7aba4608aa3f5cb6adc75
Oumaima <oumaimaamdouni69@gmail.com>
2025-03-12 01:20:28 +0100

    ajoutcours,afficherlistedescours,rechercheettridansdashbordadmin

--- Backend/routes/courses.js
+++ Backend/routes/courses.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const coursesController = require("../controllers/coursesController");
+const upload = require("../middleware/upload"); // Middleware Multer // ✅ Ajout de l'import manquant
 
 // Routes CRUD pour les cours
-router.post("/addcourses", coursesController.createCourse);
+// 📌 Route pour ajouter un cours avec un fichier PDF
+router.post("/addcourses", upload.single("file"), coursesController.addCourse);
+
+
 router.get("/getallcourses", coursesController.getAllCourses);
 router.get("/getcourses/:id", coursesController.getCourseById);
 router.put("/updatecourses/:id", coursesController.updateCourse);
