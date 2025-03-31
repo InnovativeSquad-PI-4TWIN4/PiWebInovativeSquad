36cf87903364a481db9b74509002b6dcdb911db5
ayouta2002 <eya.benslimen@esprit.tn>
2025-03-31 04:39:53 +0100

    Create messenger between users backend + interface messages

---
+++ Backend/models/Message.js
@@ -0,0 +1,15 @@
+const mongo = require("mongoose");
+const Schema = mongo.Schema;
+
+const Message = new Schema({
+    sender: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    receiver: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    read: { type: Boolean, default: false }
+}, { 
+    toJSON: { virtuals: true }, 
+    toObject: { virtuals: true }
+});
+
+module.exports = mongo.model('messages', Message);
