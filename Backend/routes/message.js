674aa3096e296b89ab0ce3b01113e1a79ae813fb
ayouta2002 <eya.benslimen@esprit.tn>
2025-04-03 01:23:42 +0100

    mark message as read

--- Backend/routes/message.js
+++ Backend/routes/message.js
@@ -8,10 +8,8 @@ router.post('/send-message', messageController.sendMessage);
 // Get all messages for a user (sent or received)
 router.get('/messages/:userId', messageController.getMessages);
 
-// Mark a message as read
-router.put('/messages/:messageId/read', messageController.markAsRead);
-
 // Get conversation messages between two users
 router.get('/conversation/:senderId/:receiverId', messageController.getConversationMessages);
+router.put('/mark-as-read/:messageId', messageController.markMessageAsRead);
 
 module.exports = router;
