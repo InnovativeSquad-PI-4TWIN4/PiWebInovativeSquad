be31d281f97c6b2645a209db0c89f8f5e0729781
Oumaima <oumaimaamdouni69@gmail.com>
2025-04-12 16:56:15 +0100

    Certificat de Succes

--- Backend/routes/examAI.routes.js
+++ Backend/routes/examAI.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { generateExam } = require("../controllers/examAI.controller");
+const { generateExam,validateExam } = require("../controllers/examAI.controller");
 
 router.post("/generate", generateExam);
+router.post("/validate", validateExam); // 👈 nouvelle route ici
 module.exports = router;
